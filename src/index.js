c115902410b57b2424121a4ab5e7d7c89f657b05
Jodu555 <Jodu505@gmail.com>
2022-03-04 11:32:08 +0100

    added the getter liknk to dl url parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const versions = {};
+
 (async () => {
+    let versions = await getVersionsAndGetterUrl();
+
+    // await getterLinkToDowmnloadUrl('https://getbukkit.org/get/68aef01121494a41fe71890b81d69d07');
+    await Promise.all(
+        versions = Object.entries(versions).map(async ([k, v]) => {
+            const downloadUrl = await getterLinkToDowmnloadUrl(v.getUrl);
+            return { ...v, downloadUrl }
+        }));
+
+    console.log(versions);
+
+})();
+
+async function getterLinkToDowmnloadUrl(url) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const downloadUrl = $($('#get-download > div > div > div:nth-child(2) > div > h2 a'))[0].attribs.href;
+    return downloadUrl;
+}
+
+async function getVersionsAndGetterUrl() {
+    const versions = [];
     const response = await axios.get(`https://getbukkit.org/download/spigot`);
     const html = response.data;
     const $ = cheerio.load(html);
@@ -10,9 +33,8 @@ const versions = {};
         // console.log($pane.html());
         const version = $pane('.row .col-sm-3 h2').text().trim();
         const getUrl = $pane('.row .col-sm-4 .btn-group .btn-download')[0].attribs.href;
-        versions[version] = { version, getUrl };
+        versions.push({ version, getUrl })
     })
-
-    console.log(versions);
-})();
+    return versions;
+}
 
